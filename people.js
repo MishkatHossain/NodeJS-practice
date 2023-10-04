7fe6c0e432fb1ea2f415a391ddea00d7279a1779
Mishkat Hossain <hossainmishkat0@gmail.com>
2023-10-04 21:47:18 +0600

    x

--- people.js
+++
@@ -1,9 +0,0 @@
-const people = ['Mishkat', 'Stark', 'Light', 'Rin']
-const maketh = [12, 13, 8+9, 20]
-
-
-
-module.exports = {
-    people: people,
-    maketh: maketh
-}
---
+++ server.js
@@ -0,0 +1,41 @@
+const http = require('http')
+const fs = require('fs');
+
+const server = http.createServer((req, res)=>{
+    
+    res.setHeader('Content-Type', 'Text/html')    
+    
+    let path = './views/';
+
+    switch(req.url) 
+    {    
+        case '/': 
+            path +='index.html'; 
+            break;
+        case '/about': 
+            path +='about.html'; 
+            break;
+        default : 
+            path +='404.html'; 
+            break;
+    }
+
+    fs.readFile(path, (err, data)=>{
+        if(err)
+            console.log(err);
+        else
+            res.write(data);
+
+        res.end()
+    })
+
+
+
+})
+
+
+server.listen(3000, 'localhost', () => {
+    console.log('Listening for request on port 3000')
+});
+
+
