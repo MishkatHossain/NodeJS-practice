77e9e58d543974d66f8067fbc2eab05eed5f93f8
Mishkat Hossain <hossainmishkat0@gmail.com>
2023-10-07 11:34:25 +0600

    grimknight

--- app.js
+++ app.js
@@ -2,3 +2,37 @@ const express = require('express');
 
 
 const app = express();
+
+// register view engine 
+
+app.set('view engine', 'ejs');
+
+
+app.listen(3000);
+
+
+app.get('/', (req, res)=>{
+    // res.send('<p> home page of the sharingan</p>');
+
+    res.render('index')
+});
+
+app.get('/about', (req, res)=>{
+    // res.send('<p> Shark </p>');
+
+    res.render('about')
+});
+
+
+app.get('/about-us', (req, res)=>{
+    res.redirect('./about');
+})
+
+app.get('/abouts', (req, res)=>{
+    res.redirect('./about');
+})
+
+
+app.use((req, res)=>{
+    res.status(404).sendFile('./views/404.html', {root: __dirname})
+})
