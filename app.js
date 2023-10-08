a2a68becb87992537ef5772bc29e5d3a4d8a01eb
Mishkat Hossain <hossainmishkat0@gmail.com>
2023-10-08 20:20:17 +0600

    com

--- app.js
+++ app.js
@@ -1,42 +1,78 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
 const app = express();
 
 //connect to mongoDB
 
-// const dbURI = 'mongodb+srv://netninja:qwerty123@nodetuts.mgzrcfl.mongodb.net/node-tuts?retryWrites=true&w=majority';
-// mongoose.connect(dbURI, {userNewUrlParse: true, useUnifiedTopology: true})
-//     .then()
+const databaseURI = 'mongodb+srv://netninja:qwerty123@nodetuts.mgzrcfl.mongodb.net/note-tuts?retryWrites=true&w=majority'
+
+mongoose.connect(databaseURI, { useNewUrlParser:true, useUnifiedTopology: true})
+.then((conn)=>app.listen(3000))
+.catch((err)=>{ console.log(err)});
+    
+
 
 app.set('view engine', 'ejs');
 
-app.listen(3000);
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 app.use(morgan('dev'))
 
 
-// middleware and static files
-
-
+//routes
 app.get('/', (req, res)=>{
-
-    const blogs = [
-        {title: 'YashidaCorp', snippet: 'Just let me crushed in piece'},
-        {title: 'One piece', snippet: 'This is One piece of naruto'},
-        {title: 'Realm', snippet: 'Attack on Yajuj Majuj'},
-        {title: 'Realm Once', snippet: 'Tom halal'},
-    ];
-
-    res.render('index', { title: 'Home', blogs})
+    res.redirect('/blogs')
 });
 
 
 app.get('/about', (req, res)=>{
     res.render('about', { title: 'About'})
 });
+
+
+//blog routes
+app.get('/blogs', (req,res)=>{
+    Blog.find().sort({createdAt: -1})
+    .then((result)=>{
+        res.render('index', {title: 'All Blogs', blogs: result})
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+});
+
+
+app.post('/blogs', (req,res)=>{
+
+    const blog = new Blog(req.body)
+
+    blog.save()
+     .then((result) => {
+       res.redirect('/blogs')
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+})
+
+app.get('/blogs/:id', (req, res)=>{
+    const id =req.params.id;
+    console.log(id);
+    Blog.findById(id)
+    .then((result)=>{
+        render('details', {blog: result, title: 'Blog Details'});
+
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+})
+
+
 
 app.get('/blogs/create', (req, res)=>{
     res.render('create',{ title: 'Create a new blog'});
